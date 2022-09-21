5c915420a4d5e373e4800f966cc598c287e6c117
md-alramlawi <md.alramlawi@gmail.com>
2022-09-21 23:00:18 +0300

    configure PlantAdapter.kt & it`s binding adapter

---
+++ LiveDataApp/src/main/java/com/ramalwi/plants/PlantAdapter.kt
@@ -0,0 +1,48 @@
+
+package com.ramalwi.plants
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ramalwi.plants.databinding.ListItemPlantBinding
+
+/**
+ * Adapter for the [RecyclerView] in [PlantListFragment].
+ */
+class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>(PlantDiffCallback()) {
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val plant = getItem(position)
+        (holder as PlantViewHolder).bind(plant)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return PlantViewHolder(
+            ListItemPlantBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false))
+    }
+
+    internal class PlantViewHolder(private val binding: ListItemPlantBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Plant) {
+            binding.apply {
+                plant = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    internal class PlantDiffCallback : DiffUtil.ItemCallback<Plant>() {
+
+        override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+            return oldItem.plantId == newItem.plantId
+        }
+
+        override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
+
