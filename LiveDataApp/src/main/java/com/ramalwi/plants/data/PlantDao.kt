c560e6df2997e7491b8655212c086e61185c880e
md-alramlawi <md.alramlawi@gmail.com>
2022-09-21 23:10:40 +0300

    create data layer

---
+++ LiveDataApp/src/main/java/com/ramalwi/plants/data/PlantDao.kt
@@ -0,0 +1,23 @@
+package com.ramalwi.plants.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ramalwi.plants.Plant
+
+/**
+ * The Data Access Object for the Plant class.
+ */
+@Dao
+interface PlantDao {
+    @Query("SELECT * FROM plants ORDER BY name")
+    fun getPlants(): LiveData<List<Plant>>
+
+    @Query("SELECT * FROM plants WHERE growZoneNumber = :growZoneNumber ORDER BY name")
+    fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): LiveData<List<Plant>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(plants: List<Plant>)
+}
---
+++ LiveDataApp/src/main/java/com/ramalwi/plants/data/PlantRepository.kt
@@ -0,0 +1,160 @@
+package com.ramalwi.plants.data
+
+import androidx.annotation.AnyThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
+import com.ramalwi.plants.GrowZone
+import com.ramalwi.plants.Plant
+import com.ramalwi.plants.util.CacheOnSuccess
+import com.ramalwi.plants.util.ComparablePair
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Repository module for handling data operations.
+ *
+ * This PlantRepository exposes two UI-observable database queries [plants] and
+ * [getPlantsWithGrowZone].
+ *
+ * To update the plants cache, call [tryUpdateRecentPlantsForGrowZoneCache] or
+ * [tryUpdateRecentPlantsCache].
+ */
+class PlantRepository private constructor(
+    private val plantDao: PlantDao,
+    private val plantService: NetworkService,
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+) {
+
+    /**
+     * Fetch a list of [Plant]s from the database.
+     * Returns a LiveData-wrapped List of Plants.
+     */
+    //val plants = plantDao.getPlants()
+
+    val plants: LiveData<List<Plant>> = liveData<List<Plant>> {
+        val plantsLiveData = plantDao.getPlants()
+        val customSortOrder = plantsListSortOrderCache.getOrAwait()
+
+        val sortedPlantsLiveData = plantsLiveData.map { plantList ->
+            plantList.applySort(customSortOrder)
+        }
+        emitSource(sortedPlantsLiveData)
+    }
+
+    /**
+     * Fetch a list of [Plant]s from the database that matches a given [GrowZone].
+     * Returns a LiveData-wrapped List of Plants.
+     */
+//    fun getPlantsWithGrowZone(growZone: GrowZone) =
+//        plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+
+//    fun getPlantsWithGrowZone(growZone: GrowZone) = liveData {
+//        val plantsGrowZoneLiveData = plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+//        val customSortOrder = plantsListSortOrderCache.getOrAwait()
+//        val sortedPlantsGrowZoneLiveData = plantsGrowZoneLiveData.map { plantList ->
+//            plantList.applySort(customSortOrder)
+//        }
+//        emitSource(sortedPlantsGrowZoneLiveData)
+//    }
+
+
+    fun getPlantsWithGrowZone(growZone: GrowZone): LiveData<List<Plant>> {
+        val plantsGrowZoneLiveData = plantDao.getPlantsWithGrowZoneNumber(growZone.number)
+        return plantsGrowZoneLiveData.switchMap {
+            liveData {
+                val customSortOrder = plantsListSortOrderCache.getOrAwait()
+                emit(it.applyMainSafeSort(customSortOrder))
+            }
+        }
+    }
+
+
+    /**
+     * Returns true if we should make a network request.
+     */
+    private fun shouldUpdatePlantsCache(): Boolean {
+        // suspending function, so you can e.g. check the status of the database here
+        return true
+    }
+
+    /**
+     * Update the plants cache.
+     *
+     * This function may decide to avoid making a network requests on every call based on a
+     * cache-invalidation policy.
+     */
+    suspend fun tryUpdateRecentPlantsCache() {
+        if (shouldUpdatePlantsCache()) fetchRecentPlants()
+    }
+
+    /**
+     * Update the plants cache for a specific grow zone.
+     *
+     * This function may decide to avoid making a network requests on every call based on a
+     * cache-invalidation policy.
+     */
+    suspend fun tryUpdateRecentPlantsForGrowZoneCache(growZoneNumber: GrowZone) {
+        if (shouldUpdatePlantsCache()) fetchPlantsForGrowZone(growZoneNumber)
+    }
+
+    /**
+     * Fetch a new list of plants from the network, and append them to [plantDao]
+     */
+    private suspend fun fetchRecentPlants() {
+        val plants = plantService.allPlants()
+        plantDao.insertAll(plants)
+    }
+
+    /**
+     * Fetch a list of plants for a grow zone from the network, and append them to [plantDao]
+     */
+    private suspend fun fetchPlantsForGrowZone(growZone: GrowZone) {
+        val plants = plantService.plantsByGrowZone(growZone)
+        plantDao.insertAll(plants)
+    }
+
+//    private var plantsListSortOrderCache =
+//        CacheOnSuccess(onErrorFallback = { listOf<String>() }) {
+//            plantService.customPlantSortOrder()
+//        }
+
+    private var plantsListSortOrderCache =
+        CacheOnSuccess(
+            onErrorFallback = { listOf() },
+            block = {
+                plantService.customPlantSortOrder()
+            }
+        )
+
+    private fun List<Plant>.applySort(customSortOrder: List<String>): List<Plant> {
+
+        return this.sortedBy { plant ->
+            val positionForItem = customSortOrder.indexOf(plant.plantId).let { order ->
+                if (order > -1) order
+                else Int.MAX_VALUE
+            }
+            ComparablePair(positionForItem, plant.name)
+        }
+    }
+
+    @AnyThread
+    suspend fun List<Plant>.applyMainSafeSort(customSortOrder: List<String>) =
+        withContext(defaultDispatcher) {
+            this@applyMainSafeSort.applySort(customSortOrder)
+        }
+
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile
+        private var instance: PlantRepository? = null
+
+        fun getInstance(plantDao: PlantDao, plantService: NetworkService) =
+            instance ?: synchronized(this) {
+                instance ?: PlantRepository(plantDao, plantService).also { instance = it }
+            }
+    }
+}
